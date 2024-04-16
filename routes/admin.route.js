6422b857b8ba3374c5a6c57a9859bd6a5b6103c6
David Raviv <david@raviv.co>
2024-04-16 13:08:32 +0300

    adjust admin route limits

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,8 +6,8 @@ const adminController = require('../controllers/admin.controller');
 
 // Enable rate limiter
 const limiter = rateLimit({
-  windowMs: 3 * 60 * 1000, // 3 minutes
-  max: 1 // limit each IP to 1 requests per windowMs 
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 3 // limit each IP to 3 requests per windowMs 
 });
 
 router.post('/loadStations', limiter, adminController.loadStations);
